2556e260e78aa0f1057f1127f05fb354ca9ef049
Andres Aizaga <andres.aizaga@devsu.com>
2022-10-27 15:14:00 -0500

    added database project

---
+++ HAHN.Database/dbo/Tables/Blogs.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Blogs] (
+    [BlogId] INT           IDENTITY (1, 1) NOT NULL,
+    [Url]    VARCHAR (250) NULL,
+    [Rating] INT           NULL,
+    CONSTRAINT [PK_Blogs] PRIMARY KEY CLUSTERED ([BlogId] ASC)
+);
+
---
+++ HAHN.Database/dbo/Tables/Contact.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Contact] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [Name]  VARCHAR (250) NULL,
+    [Email] VARCHAR (250) NULL,
+    [Phone] VARCHAR (50)  NULL,
+    [Age]   INT           NULL,
+    CONSTRAINT [PK_Contact] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
